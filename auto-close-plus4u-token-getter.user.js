93ff14eb7f4695ef9cf70b9e1f9530f3cef6b89d
uumnk <56532513+uumnk@users.noreply.github.com>
2023-07-11 19:50:44 +0200

    Extend matched URL

--- auto-close-plus4u-token-getter.user.js
+++ auto-close-plus4u-token-getter.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Auto close Plus4U token getter
 // @namespace    https://github.com/uumnk/auto-close-plus4u-token-getter
-// @version      0.1
+// @version      0.2
 // @description  Auto close the page for getting token by insomnia-plugin-plus4u-oidc.
 // @author       Monika
-// @match        https://uuidentity.plus4u.net/uu-identitymanagement-maing01/a9b105aff2744771be4daa8361954677/showAuthorizationCode?clientId=F64b643R05xQ62696875W1j2
+// @match        https://uuidentity.plus4u.net/uu-identitymanagement-maing01/a9b105aff2744771be4daa8361954677/showAuthorizationCode?clientId=*
 // @grant        none
 // ==/UserScript==
 
